d6438cb279b80cf2e13b0d546bedffe328ddfb4c
Selman <selmankayrancioglu@gmail.com>
2023-04-16 16:43:48 +0300

    zshrc.d/fzf: add bindings

--- dot_zshrc.d/007-fzf.zsh
+++ dot_zshrc.d/007-fzf.zsh
@@ -3,3 +3,10 @@
 export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --inline-info --border'
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --exclude .git'
 
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/Users/selman/.fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/Users/selman/.fzf/shell/key-bindings.zsh"
