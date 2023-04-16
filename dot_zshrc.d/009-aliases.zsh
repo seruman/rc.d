73dde210870fb7c101240dfb06c634add1e2f58f
Selman <selmankayrancioglu@gmail.com>
2023-04-16 16:44:18 +0300

    .zshrc.d/aliases: update ghqf with preview and empty input

--- dot_zshrc.d/009-aliases.zsh
+++ dot_zshrc.d/009-aliases.zsh
@@ -1,11 +1,28 @@
 # vi: ft=zsh
 
-alias ghqf='cd $(ghq list | fzf | xargs -I{} echo $(ghq root)/{})'
+function ghqf() {
+    # TODO(selman): gum is unnecessary IMHO. 
+    local previewcmd="
+    gum format -t template '{{ Bold \"{}\" }}';
+    git -C \$(ghq list --full-path -e {}) remote get-url \$(git -C \$(ghq list --full-path -e {}) remote);
+    ls --color=always \$(ghq list --full-path -e {});
+    "
+    local repopath=$(
+        ghq list |
+            fzf \
+                --prompt 'Repositories> ' \
+                --preview "${previewcmd}" \
+                --query "${*:-}" |
+            xargs -I{} ghq list --full-path -e {}
+    )
+
+    if [ -n "$repopath" ]; then
+        cd "$repopath"
+    fi
+}
 
 # Git
 alias gr='cd `git root`'
-
-
 
 # OpenScad: macOS
 if [[ "$OS" == "Darwin" ]]; then
@@ -13,4 +30,4 @@ if [[ "$OS" == "Darwin" ]]; then
     if [[ ! -f "$openscad_dir" ]]; then :; fi
 
     alias openscad="$openscad_dir/OpenSCAD"
-fi  
+fi
