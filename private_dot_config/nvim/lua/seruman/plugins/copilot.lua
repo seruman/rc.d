a0ef3fe3648509e10dd9f294d4071bdaf9de52db
Selman <selmankayrancioglu@gmail.com>
2023-05-01 22:40:38 +0300

    .config/nvim/.../copilot: add copilot

---
+++ private_dot_config/nvim/lua/seruman/plugins/copilot.lua
@@ -0,0 +1,43 @@
+return {
+    {
+        "github/copilot.vim",
+        init = function()
+            vim.g.copilot_no_tab_map = true
+            vim.g.copilot_assume_mapped = true
+            vim.g.copilot_tab_fallback = ''
+        end,
+        config = function()
+            vim.keymap.set(
+                "i", "<C-T>",
+                'copilot#Accept("<CR>")',
+                {
+                    silent = true,
+                    expr = true,
+                    script = true,
+                    replace_keycodes = false,
+                    desc = "Copilot accept suggestion",
+                })
+
+            vim.keymap.set(
+                "i", "<C-C><C-]>",
+                'copilot#Next()',
+                {
+                    silent = true,
+                    expr = true,
+                    script = true,
+                    replace_keycodes = false,
+                    desc = "Copilot next suggestion",
+                })
+
+            vim.keymap.set(
+                "i", "<C-C><C-[>",
+                'copilot#Previous()',
+                {
+                    silent = true,
+                    expr = true,
+                    replace_keycodes = false,
+                    desc = "Copilot previous suggestion",
+                })
+        end
+    }
+}
