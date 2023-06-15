b4169772413b27cb58272199180b0fadb0de7b6f
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2023-06-16 00:15:01 +0300

    .config/nvim: replace splitjoin with treesj

---
+++ private_dot_config/nvim/lua/seruman/plugins/treesj.lua
@@ -0,0 +1,37 @@
+return {
+    {
+        'Wansmer/treesj',
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter',
+        },
+        config = function()
+            require('treesj').setup({
+                use_default_keymaps = false,
+                cursor_behavior = 'end',
+            })
+        end,
+        keys = {
+            {
+                'gJ',
+                function()
+                    require('treesj').join()
+                end,
+                desc = 'Join the object under cursor',
+            },
+            {
+                'gS',
+                function()
+                    require('treesj').split()
+                end,
+                desc = 'Split the object under cursor',
+            },
+            {
+                'gT',
+                function()
+                    require('treesj').toggle()
+                end,
+                desc = 'Toggle split/join of the object under cursor',
+            }
+        },
+    }
+}
