6637659b29a05d641c7680090e0fa6289cfb280b
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2023-07-02 18:03:25 +0300

    .config/nvim/.../plugins: add leap

---
+++ private_dot_config/nvim/lua/seruman/plugins/leap.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        "ggandor/leap.nvim",
+        keys = {
+            { 'ss', '<Plug>(leap-forward-to)',  mode = { 'n', 'x', 'o' }, desc = 'Leap forward to' },
+            { 'sS', '<Plug>(leap-backward-to)', mode = { 'n', 'x', 'o' }, desc = 'Leap backward to' },
+            { 'SS', '<Plug>(leap-from-window)', mode = { 'n', 'x', 'o' }, desc = 'Leap from windows' },
+        },
+    }
+}
