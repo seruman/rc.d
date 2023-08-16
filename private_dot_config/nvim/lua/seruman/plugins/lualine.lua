cd55d5567d9a3a78bcb8851019fdbddbec877ad6
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2023-08-17 00:24:53 +0300

    .config/nvim/.../lualine: use custom melange theme

--- private_dot_config/nvim/lua/seruman/plugins/lualine.lua
+++ private_dot_config/nvim/lua/seruman/plugins/lualine.lua
@@ -5,6 +5,58 @@ return {
         --     "kyazdani42/nvim-web-devicons",
         -- },
         opts = function()
+            local function theme_melange()
+                -- NOTE(selman): Kinda ripped from;
+                -- https://github.com/nvim-lualine/lualine.nvim/wiki/Writing-a-theme
+                local bg = vim.opt.background:get()
+                local palette = require('melange/palettes/' .. bg)
+
+                local a = palette.a -- Grays
+                local b = palette.b -- Bright foreground colors
+                local c = palette.c -- Foreground colors
+                local d = palette.d -- Background colors
+
+
+                return {
+
+                    normal = {
+                        a = { bg = a.com, fg = a.bg },
+                        b = { bg = a.bg, fg = a.com },
+                        c = { bg = a.float, fg = a.com },
+                    },
+                    insert = {
+                        a = { bg = c.green, fg = a.bg },
+                        b = { bg = a.bg, fg = c.green },
+                        c = { bg = a.float, fg = c.green },
+                    },
+                    command = {
+                        a = { bg = c.yellow, fg = a.bg },
+                        b = { bg = a.bg, fg = c.yellow },
+                        c = { bg = a.float, fg = c.yellow },
+                    },
+                    visual = {
+                        a = { bg = c.magenta, fg = a.bg },
+                        b = { bg = a.bg, fg = c.magenta },
+                        c = { bg = a.float, fg = c.magenta },
+                    },
+                    replace = {
+                        a = { bg = c.green, fg = a.bg },
+                        b = { bg = a.bg, fg = c.green },
+                        c = { bg = a.float, fg = c.green },
+                    },
+                    terminal = {
+                        a = { bg = c.yellow, fg = a.bg },
+                        b = { bg = a.bg, fg = c.yellow },
+                        c = { bg = a.float, fg = c.yellow },
+                    },
+                    inactive = {
+                        a = { bg = a.com, fg = a.bg },
+                        b = { bg = a.bg, fg = a.com, gui = 'bold' },
+                        c = { bg = a.float, fg = a.com },
+                    },
+                }
+            end
+
             local function strsplit(s, delimiter)
                 local result = {};
                 for match in (s .. delimiter):gmatch("(.-)" .. delimiter) do
@@ -26,7 +78,8 @@ return {
             return {
                 options = {
                     -- TODO(selman): Melange support.
-                    theme = "solarized_light",
+                    -- theme = "solarized_light",
+                    theme = theme_melange(),
                     icons_enabled = false,
                     component_separators = { left = "|", right = "|" },
                     section_separators = { left = "", right = "" },
