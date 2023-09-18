fa60ea5851709a7119899d77dd208f01e0092fdc
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2023-09-18 17:15:27 +0300

    .zshrc.d/darwin.d: more defaults

--- dot_zshrc.d/darwin.d/000-defaults.zsh
+++ dot_zshrc.d/darwin.d/000-defaults.zsh
@@ -13,10 +13,13 @@ defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
 
 # TODO(selman): https://wilsonmar.github.io/dotfiles/
 # TODO(selman): https://github.com/mathiasbynens/dotfiles/blob/main/.macos
+# TODO(selman): https://raw.githubusercontent.com/rusty1s/dotfiles/master/macos/defaults.sh
 defaults write com.apple.Finder QuitMenuItem 1
 defaults write com.apple.finder ShowStatusBar -bool true
 defaults write com.apple.finder ShowPathbar -bool true
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES
+defaults write com.apple.finder NewWindowTarget -string "PfHm"
+defaults write com.apple.finder FXPreferredViewStyle clmv
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
-defaults write com.apple.finder NewWindowTarget -string "PfDe"
-defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
+defaults write com.apple.screencapture location -string "${HOME}/etc/screenshots"
