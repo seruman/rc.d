a72707a3708122bac4647f2067463f5daf1f3010
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2023-09-25 10:25:00 +0300

    .config/nvim: add octo.nvim

---
+++ private_dot_config/nvim/lua/seruman/plugins/octo.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        'pwntester/octo.nvim',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+        },
+        config = function()
+            require("octo").setup({
+                picker = 'fzf-lua'
+            })
+        end,
+    },
+}
