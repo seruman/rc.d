b6d9cc5ccca21c933b286e611613487ab2350e97
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2023-10-05 17:06:18 +0300

    .config/nvim/.../nvim-lspconfig: update lua_ls setup

--- private_dot_config/nvim/lua/seruman/plugins/lsp/nvim-lspconfig.lua
+++ private_dot_config/nvim/lua/seruman/plugins/lsp/nvim-lspconfig.lua
@@ -127,23 +127,32 @@ return {
             end
 
             local function setup_lua_ls()
-                -- TODO(selman): Does not recognize nvim stuff.
+                local runtime_path = vim.split(package.path, ';')
+                table.insert(runtime_path, 'lua/?.lua')
+                table.insert(runtime_path, 'lua/?/init.lua')
+
                 return {
                     settings = {
                         Lua = {
+                            -- Disable telemetry
+                            telemetry = { enable = false },
                             runtime = {
+                                -- Tell the language server which version of Lua you're using
+                                -- (most likely LuaJIT in the case of Neovim)
                                 version = 'LuaJIT',
-                                path = vim.split(package.path, ';'),
+                                path = runtime_path,
                             },
                             diagnostics = {
-                                globals = { 'vim' },
+                                -- Get the language server to recognize the `vim` global
+                                globals = { 'vim' }
                             },
                             workspace = {
-                                -- Make the server aware of Neovim runtime files
-                                library = vim.api.nvim_get_runtime_file("", true),
-                            },
-                            telemetry = {
-                                enable = false,
+                                checkThirdParty = false,
+                                library = {
+                                    -- Make the server aware of Neovim runtime files
+                                    vim.fn.expand('$VIMRUNTIME/lua'),
+                                    vim.fn.stdpath('config') .. '/lua'
+                                }
                             }
                         }
                     }
