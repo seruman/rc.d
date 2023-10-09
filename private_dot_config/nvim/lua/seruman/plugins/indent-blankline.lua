115aa4cb557da47d39e5f5f35830b116c66c047d
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2023-10-09 13:56:04 +0300

    .config/nvim/.../indent-blankline: update setup for new version

--- private_dot_config/nvim/lua/seruman/plugins/indent-blankline.lua
+++ private_dot_config/nvim/lua/seruman/plugins/indent-blankline.lua
@@ -1,3 +1,20 @@
 return {
-    { "lukas-reineke/indent-blankline.nvim" },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        config = function()
+            require("ibl").setup {
+                enabled = false,
+                indent = {
+                    highlight = 'IndentBlanklineChar',
+                    -- char = "",
+                },
+                whitespace = {
+                    highlight = 'IndentBlanklineSpaceChar',
+                    remove_blankline_trail = false,
+                },
+                scope = { enabled = false },
+            }
+        end,
+    },
 }
