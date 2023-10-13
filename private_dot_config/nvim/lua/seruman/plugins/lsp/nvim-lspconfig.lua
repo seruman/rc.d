72439ed8f8b9e973a7127e1bf5e727c3471254ba
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2023-10-13 14:17:22 +0300

    .config/nvim/.../nvim-lspconfig: read project specific yaml schemas from env

--- private_dot_config/nvim/lua/seruman/plugins/lsp/nvim-lspconfig.lua
+++ private_dot_config/nvim/lua/seruman/plugins/lsp/nvim-lspconfig.lua
@@ -209,13 +209,25 @@ return {
 
 
             local function setup_yamlls()
+                local schemas = {}
+                local env_project_schemas = vim.env.YAMLLS_PROJECT_SCHEMAS
+                if env_project_schemas ~= nil then
+                    local ok, decoded = pcall(vim.fn.json_decode, env_project_schemas)
+                    if ok then
+                        for _, schema in ipairs(decoded) do
+                            for uri, matcher in pairs(schema) do
+                                schemas[uri] = matcher
+                            end
+                        end
+                    end
+                end
+
                 return {
                     settings = {
                         yaml = {
+                            redhat = { telemetry = { enable = false } },
                             keyOrdering = false,
-                            schemaStore = {
-                                enable = true
-                            },
+                            schemas = schemas,
                         }
                     }
                 }
