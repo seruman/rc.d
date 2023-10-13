4af02689d5b5ab314af54282b1574033a1be6a60
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2023-10-13 09:05:02 +0300

    .config/nvim/.../colorscheme: add highlights for LspReference*

--- private_dot_config/nvim/lua/seruman/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/seruman/plugins/colorscheme.lua
@@ -7,6 +7,33 @@ return {
             vim.o.termguicolors = true
             vim.o.background = "light"
             vim.cmd("colorscheme melange")
+
+            local SearchHighlight = vim.api.nvim_get_hl(0, { name = "Search" })
+            local TSLSPTypeNamespaceHighlight = vim.api.nvim_get_hl(0, { name = "@lsp.type.namespace" })
+            vim.api.nvim_set_hl(0,
+                "LspReferenceRead", {
+                    fg = SearchHighlight.fg,
+                    bg = TSLSPTypeNamespaceHighlight.fg,
+                    bold = true,
+                    undercurl = true,
+                }
+            )
+            vim.api.nvim_set_hl(0,
+                "LspReferenceText", {
+                    fg = SearchHighlight.fg,
+                    bg = TSLSPTypeNamespaceHighlight.fg,
+                    bold = true,
+                    undercurl = true,
+                }
+            )
+            vim.api.nvim_set_hl(0,
+                "LspReferenceWrite", {
+                    fg = SearchHighlight.fg,
+                    bg = TSLSPTypeNamespaceHighlight.fg,
+                    bold = true,
+                    undercurl = true,
+                }
+            )
         end,
     },
 }
