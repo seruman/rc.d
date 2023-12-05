08ec1e52ea585ab131142a08b8e1fc37af8b7315
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2023-12-05 15:27:58 +0300

    .config/nvim: add nvim-goc

---
+++ private_dot_config/nvim/lua/seruman/plugins/nvim-goc.lua
@@ -0,0 +1,18 @@
+return {
+    {
+        "rafaelsq/nvim-goc.lua",
+        dependencies = {
+        },
+        config = function()
+            vim.opt.switchbuf = 'useopen'
+
+            local goc = require 'nvim-goc'
+            goc.setup({ verticalSplit = false }) -- default to horizontal
+
+
+            vim.keymap.set('n', '<Leader>gcf', goc.Coverage, { silent = true })      -- run for the whole File
+            vim.keymap.set('n', '<Leader>gct', goc.CoverageFunc, { silent = true })  -- run only for a specific Test unit
+            vim.keymap.set('n', '<Leader>gcc', goc.ClearCoverage, { silent = true }) -- clear coverage highlights
+        end,
+    }
+}
