52cdbca3fe529c765bead4f43d52ead574a9af39
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2023-12-13 14:24:06 +0300

    .config/nvim/.../treesitter: increase max filesize to ~500KB

--- private_dot_config/nvim/lua/seruman/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/seruman/plugins/treesitter.lua
@@ -46,7 +46,7 @@ return {
                     enable = true,
                     -- Disable highlights for large files
                     disable = function(lang, buf)
-                        local max_filesize = 100 * 1024 -- 100 KB
+                        local max_filesize = 500 * 1024 -- 500 KB
                         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
                         if ok and stats and stats.size > max_filesize then
                             return true
