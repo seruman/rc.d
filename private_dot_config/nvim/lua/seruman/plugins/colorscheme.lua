bf9708e80d038e72d714ec0f7ada272439ae3b74
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2024-02-18 18:40:42 +0300

    {ghostty,nvim,tmux}: try zenbones

--- private_dot_config/nvim/lua/seruman/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/seruman/plugins/colorscheme.lua
@@ -1,39 +1,44 @@
 return {
     {
-        "seruman/melange-nvim",
+        -- "seruman/melange-nvim",
+        "mcchrish/zenbones.nvim",
+        dependencies = {
+            "rktjmp/lush.nvim",
+        },
         lazy = false,
         priority = 1000,
         config = function()
             vim.o.termguicolors = true
             vim.o.background = "light"
-            vim.cmd("colorscheme melange")
-
-            local SearchHighlight = vim.api.nvim_get_hl(0, { name = "Search" })
-            local TSLSPTypeNamespaceHighlight = vim.api.nvim_get_hl(0, { name = "@lsp.type.namespace" })
-            vim.api.nvim_set_hl(0,
-                "LspReferenceRead", {
-                    fg = SearchHighlight.fg,
-                    bg = TSLSPTypeNamespaceHighlight.fg,
-                    bold = true,
-                    undercurl = true,
-                }
-            )
-            vim.api.nvim_set_hl(0,
-                "LspReferenceText", {
-                    fg = SearchHighlight.fg,
-                    bg = TSLSPTypeNamespaceHighlight.fg,
-                    bold = true,
-                    undercurl = true,
-                }
-            )
-            vim.api.nvim_set_hl(0,
-                "LspReferenceWrite", {
-                    fg = SearchHighlight.fg,
-                    bg = TSLSPTypeNamespaceHighlight.fg,
-                    bold = true,
-                    undercurl = true,
-                }
-            )
+            vim.cmd("colorscheme zenbones")
+            -- vim.cmd("colorscheme melange")
+            --
+            -- local SearchHighlight = vim.api.nvim_get_hl(0, { name = "Search" })
+            -- local TSLSPTypeNamespaceHighlight = vim.api.nvim_get_hl(0, { name = "@lsp.type.namespace" })
+            -- vim.api.nvim_set_hl(0,
+            --     "LspReferenceRead", {
+            --         fg = SearchHighlight.fg,
+            --         bg = TSLSPTypeNamespaceHighlight.fg,
+            --         bold = true,
+            --         undercurl = true,
+            --     }
+            -- )
+            -- vim.api.nvim_set_hl(0,
+            --     "LspReferenceText", {
+            --         fg = SearchHighlight.fg,
+            --         bg = TSLSPTypeNamespaceHighlight.fg,
+            --         bold = true,
+            --         undercurl = true,
+            --     }
+            -- )
+            -- vim.api.nvim_set_hl(0,
+            --     "LspReferenceWrite", {
+            --         fg = SearchHighlight.fg,
+            --         bg = TSLSPTypeNamespaceHighlight.fg,
+            --         bold = true,
+            --         undercurl = true,
+            --     }
+            -- )
         end,
     },
 }
