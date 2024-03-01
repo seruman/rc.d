d2c9289b0bf38c862892fe8950cac0a9aa676a57
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2024-03-01 23:54:52 +0300

    .config/nvim: update completion mapping, make format silent

--- private_dot_config/nvim/lua/seruman/plugins/lsp/nvim-cmp.lua
+++ private_dot_config/nvim/lua/seruman/plugins/lsp/nvim-cmp.lua
@@ -28,7 +28,7 @@ return {
                     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
                     ['<C-f>'] = cmp.mapping.scroll_docs(4),
                     ['<C-e>'] = cmp.mapping.abort(),
-                    ['<CR>'] = cmp.mapping.confirm { select = true },
+                    ['<C-k>'] = cmp.mapping.confirm { select = true },
                 },
                 view = {
                     entries = {
