2320fc61faab7cca9811571e33289559aa517d37
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2024-03-07 19:22:27 +0300

    .config/nvim/.../nvim-highlight-colors: disable by default

--- private_dot_config/nvim/lua/seruman/plugins/nvim-highlight-colors.lua
+++ private_dot_config/nvim/lua/seruman/plugins/nvim-highlight-colors.lua
@@ -2,7 +2,10 @@ return {
 	{
 		"brenoprata10/nvim-highlight-colors",
 		opts = {
-            render = "virtual",
-        },
+			render = "virtual",
+		},
+		config = function()
+			require("nvim-highlight-colors").turnOff()
+		end,
 	},
 }
