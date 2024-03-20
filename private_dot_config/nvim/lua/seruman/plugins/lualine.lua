66429c50dba3609422547ed2d124bb759ced972a
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2024-03-20 23:10:40 +0300

    .config/nvim: update melange, use melange in lualine

--- private_dot_config/nvim/lua/seruman/plugins/lualine.lua
+++ private_dot_config/nvim/lua/seruman/plugins/lualine.lua
@@ -6,56 +6,6 @@ return {
 			"kyazdani42/nvim-web-devicons",
 		},
 		opts = function()
-			local function theme_melange()
-				-- NOTE(selman): Kinda ripped from;
-				-- https://github.com/nvim-lualine/lualine.nvim/wiki/Writing-a-theme
-				local bg = vim.opt.background:get()
-				local palette = require("melange/palettes/" .. bg)
-
-				local a = palette.a -- Grays
-				local b = palette.b -- Bright foreground colors
-				local c = palette.c -- Foreground colors
-				local d = palette.d -- Background colors
-
-				return {
-					normal = {
-						a = { bg = a.com, fg = a.bg },
-						b = { bg = a.bg, fg = a.com },
-						c = { bg = a.float, fg = a.com },
-					},
-					insert = {
-						a = { bg = c.green, fg = a.bg },
-						b = { bg = a.bg, fg = c.green },
-						c = { bg = a.float, fg = c.green },
-					},
-					command = {
-						a = { bg = c.yellow, fg = a.bg },
-						b = { bg = a.bg, fg = c.yellow },
-						c = { bg = a.float, fg = c.yellow },
-					},
-					visual = {
-						a = { bg = c.magenta, fg = a.bg },
-						b = { bg = a.bg, fg = c.magenta },
-						c = { bg = a.float, fg = c.magenta },
-					},
-					replace = {
-						a = { bg = c.green, fg = a.bg },
-						b = { bg = a.bg, fg = c.green },
-						c = { bg = a.float, fg = c.green },
-					},
-					terminal = {
-						a = { bg = c.yellow, fg = a.bg },
-						b = { bg = a.bg, fg = c.yellow },
-						c = { bg = a.float, fg = c.yellow },
-					},
-					inactive = {
-						a = { bg = d.bg, fg = d.fg },
-						b = { bg = d.bg, fg = d.com, gui = "bold" },
-						c = { bg = d.float, fg = d.com },
-					},
-				}
-			end
-
 			local function strsplit(s, delimiter)
 				local result = {}
 				for match in (s .. delimiter):gmatch("(.-)" .. delimiter) do
@@ -75,7 +25,7 @@ return {
 
 			return {
 				options = {
-					theme = theme_melange(),
+					theme = "melange",
 					icons_enabled = false,
 					component_separators = { left = "|", right = "|" },
 					section_separators = { left = "", right = "" },
