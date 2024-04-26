30f1dea415a3c45fa8b603daccbed1f3377e7c74
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2024-04-26 14:21:11 +0300

    .zshrc.d/005-zsh-bindkey: add semtag widget

--- dot_zshrc.d/005-zsh-bindkey.zsh
+++ dot_zshrc.d/005-zsh-bindkey.zsh
@@ -14,3 +14,30 @@ bindkey "^[[1;3D" backward-word
 autoload -U edit-command-line
 zle -N edit-command-line
 bindkey '^x^e' edit-command-line
+
+
+function _gitsemtag(){
+    if [[ ! -d .git ]]; then
+        zle reset-prompt
+        return 0
+    fi
+
+    local tags=$(git semtag -r -ii -fc)
+    if [[ -z $tags ]]; then
+        zle reset-prompt
+        return 0
+    fi
+
+    local selection=$(echo $tags | fzf --ansi --height 40% --reverse --prompt="semtag> " --preview="git show --color {1}")
+    if [[ -z $selection ]]; then
+        zle reset-prompt
+        return 0
+    fi
+
+    LBUFFER="${LBUFFER}${selection}"
+    zle reset-prompt
+    return 0
+}
+
+zle -N _gitsemtag{,}
+bindkey '^x^t' _gitsemtag
