8cfa1a7fe357e3ae8feecf18a577c46aae758e85
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2024-05-27 18:11:31 +0300

    .config/nvim/init: format

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,30 +1,27 @@
 -- Set up plugin mananager; lazy.nvim.
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup(
-    {
-        spec = {
-            { import = "seruman/plugins" },
-            { import = "seruman/plugins.lsp" },
-        },
-        change_detection = {
-            enabled = false,
-            notify = false,
-        },
-    }
-)
-
+require("lazy").setup({
+	spec = {
+		{ import = "seruman/plugins" },
+		{ import = "seruman/plugins.lsp" },
+	},
+	change_detection = {
+		enabled = false,
+		notify = false,
+	},
+})
 
 vim.cmd("syntax on")
 vim.o.hidden = true
@@ -50,9 +47,9 @@ vim.keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "Quit" })
 vim.keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "Write" })
 vim.keymap.set("n", "<leader>nh", "<cmd>nohl<CR>", { desc = "Clear highlights" })
 vim.keymap.set("n", "<c-l>", "<cmd>set cursorline!<CR>", { desc = "Toggle cursorline" })
-vim.keymap.set('n', 'n', 'nzzzv', { desc = "Move to next search match and center screen" })
-vim.keymap.set('n', 'N', 'Nzzzv', { desc = "Move to previous search match and center screen" })
-vim.keymap.set('n', '*', '*N', { desc = "Highlight without jumping forward" })
+vim.keymap.set("n", "n", "nzzzv", { desc = "Move to next search match and center screen" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Move to previous search match and center screen" })
+vim.keymap.set("n", "*", "*N", { desc = "Highlight without jumping forward" })
 vim.keymap.set("t", "<leader><esc>", "<c-\\><c-n>", { desc = "Switch to normal mode in terminal buffer" })
 vim.keymap.set("n", "<leader>bd", "<cmd>bd<CR>", { desc = "Delete buffer" })
 vim.keymap.set("n", "<A-j>", "<Cmd>resize -2<CR>", { desc = "Resize split down" })
@@ -64,11 +61,10 @@ vim.keymap.set("n", "vy", "^vg_y", { desc = "Yank to end of line" })
 vim.keymap.set("n", "vv", "^vg_", { desc = "Visually select to end of line" })
 
 local yank_current_filepath = function()
-    local fpath = vim.fn.expand('%');
-    vim.fn.setreg('+', fpath);
+	local fpath = vim.fn.expand("%")
+	vim.fn.setreg("+", fpath)
 end
 vim.keymap.set("n", "<leader><C-g>p", yank_current_filepath, { desc = "Yank current file path to unnamed register" })
-
 
 vim.cmd("inoreabbrev TODO TODO(selman):")
 vim.cmd("inoreabbrev NOTE NOTE(selman):")
@@ -114,15 +110,15 @@ vim.api.nvim_create_autocmd('WinLeave', {
 })
 
 vim.filetype.add({
-    filename = { [".swcrc"] = "json", },
-    pattern = {
-        ["%.env%.[%w_.-]+"] = "dotenv",
-        ["%.env"] = "dotenv",
-        ["%.envrc"] = "sh",
-        ["%.envrc%.?[%w_.-]*"] = "sh",
-    }
+	filename = { [".swcrc"] = "json" },
+	pattern = {
+		["%.env%.[%w_.-]+"] = "dotenv",
+		["%.env"] = "dotenv",
+		["%.envrc"] = "sh",
+		["%.envrc%.?[%w_.-]*"] = "sh",
+	},
 })
 
 if vim.env.GHOSTTY_RESOURCES_DIR and vim.fn.isdirectory(vim.env.GHOSTTY_RESOURCES_DIR .. "/vim") then
-    vim.opt.runtimepath:append(vim.env.GHOSTTY_RESOURCES_DIR .. "/vim")
+	vim.opt.runtimepath:append(vim.env.GHOSTTY_RESOURCES_DIR .. "/vim")
 end
