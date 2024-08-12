875aff2178e75fc1fe5e688ed3101c52f4024cbe
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2024-08-12 15:33:45 +0300

    .config/nvim/lua/.../lspconfig: update conform config

--- private_dot_config/nvim/lua/seruman/plugins/lsp/nvim-lspconfig.lua
+++ private_dot_config/nvim/lua/seruman/plugins/lsp/nvim-lspconfig.lua
@@ -13,13 +13,13 @@ return {
 				opts = {
 					formatters_by_ft = {
 						lua = { "stylua" },
-						typescript = { { "prettierd", "prettier" } },
-						typescriptreact = { { "prettierd", "prettier" } },
-						javascript = { { "prettierd", "prettier" } },
-						javascriptreact = { { "prettierd", "prettier" } },
-						json = { { "prettierd", "prettier" } },
-						html = { { "prettierd", "prettier" } },
-						css = { { "prettierd", "prettier" } },
+						typescript = { "prettierd", "prettier", stop_after_first = true },
+						typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+						javascript = { "prettierd", "prettier", stop_after_first = true },
+						javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+						json = { "prettierd", "prettier", stop_after_first = true },
+						html = { "prettierd", "prettier", stop_after_first = true },
+						css = { "prettierd", "prettier", stop_after_first = true },
 						sh = { "shfmt" },
 						bash = { "shfmt" },
 						go = { "goimports", "gofumpt" },
@@ -40,11 +40,13 @@ return {
 							end,
 						},
 					},
-					format_on_save = {
-						-- These options will be passed to conform.format()
-						timeout_ms = 500,
-						lsp_fallback = true,
-					},
+					format_on_save = function(bufnr)
+						-- Disable with a global or buffer-local variable
+						if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+							return
+						end
+						return { timeout_ms = 500, lsp_format = "fallback" }
+					end,
 				},
 			},
 		},
@@ -60,6 +62,24 @@ return {
 				end
 				require("conform").format({ async = true, lsp_fallback = true, range = range })
 			end, { range = true })
+
+			vim.api.nvim_create_user_command("FormatDisable", function(args)
+				if args.bang then
+					-- FormatDisable! will disable formatting just for this buffer
+					vim.b.disable_autoformat = true
+				else
+					vim.g.disable_autoformat = true
+				end
+			end, {
+				desc = "Disable autoformat-on-save",
+				bang = true,
+			})
+			vim.api.nvim_create_user_command("FormatEnable", function()
+				vim.b.disable_autoformat = false
+				vim.g.disable_autoformat = false
+			end, {
+				desc = "Re-enable autoformat-on-save",
+			})
 
 			local function setup_gopls()
 				-- Format on save.
