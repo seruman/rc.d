b9c229aff91d0938c31a2dabb4ea43b75c8c2712
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2024-12-09 09:26:45 +0300

    .config/nvim/.../octo: make it lazy with cmd

--- private_dot_config/nvim/lua/seruman/plugins/octo.lua
+++ private_dot_config/nvim/lua/seruman/plugins/octo.lua
@@ -1,13 +1,14 @@
 return {
-    {
-        'pwntester/octo.nvim',
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-        },
-        config = function()
-            require("octo").setup({
-                picker = 'fzf-lua'
-            })
-        end,
-    },
+	{
+		"pwntester/octo.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			require("octo").setup({
+				picker = "fzf-lua",
+			})
+		end,
+		cmd = "Octo",
+	},
 }
