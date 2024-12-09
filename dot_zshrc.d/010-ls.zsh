6c454c62b6cdc09212bd91f2de76ebb6381f3ce3
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2024-12-09 09:28:35 +0300

    .zshrc.d/ls: use gnu dircolors

--- dot_zshrc.d/010-ls.zsh
+++ dot_zshrc.d/010-ls.zsh
@@ -1,9 +1,11 @@
 # vi: ft=zsh
 
 if $(command -v dircolors >/dev/null 2>&1); then
-	eval $(dircolors)
-else
-	export CLICOLOR=1
+    eval $(dircolors)
+elif $(command -v gdircolors >/dev/null 2>&1); then
+    eval $(gdircolors)
 fi
+
+export CLICOLOR=1
 
 alias ls='ls --color=auto'
