623687941721a4ec8d507956129d2049041be58e
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2025-01-12 20:33:02 +0300

    .config/fish: fix edit_command_buffer binding

--- private_dot_config/private_fish/functions/fish_user_key_bindings.fish
+++ private_dot_config/private_fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,6 @@
 function fish_user_key_bindings
     set -g fish_sequence_key_delay_ms 300
 
-    bind \cx\ce edit_command_buffery
+    bind \cx\ce edit_command_buffer
     bind \cl\ct __fish_git_semtag
 end
