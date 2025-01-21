a10e9c59f1f2af07e4469895a5a9f72e85a49070
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2025-01-21 10:28:38 +0300

    .config/fish: add make shift plugin support

---
+++ private_dot_config/private_fish/conf.d/019-plugins.fish
@@ -0,0 +1,25 @@
+# TODO: Maybe configure fisher to work in its own directory?
+
+set -l current_dir (status dirname)
+set -l plugins_dir $current_dir/../plugins
+
+for plugin in $plugins_dir/*
+    if not test -d $plugin
+        continue
+    end
+
+    # if has functions, add to fish_function_path
+    if test -d $plugin/functions
+        set -g fish_function_path $fish_function_path $plugin/functions
+    end
+
+    if test -d $plugin/completions
+        set -g fish_complete_path $fish_complete_path $plugin/completions
+    end
+
+    if test -d $plugins/conf.d
+        for conf in $plugin/conf.d/*
+            source $conf
+        end
+    end
+end
