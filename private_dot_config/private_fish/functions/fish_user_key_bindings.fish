ecd45e96279c5486a167ba4d2610c34a164160fb
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2025-02-27 23:15:52 +0300

    .config/fish: update bindings for v4.0.0

--- private_dot_config/private_fish/functions/fish_user_key_bindings.fish
+++ private_dot_config/private_fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,7 @@
 function fish_user_key_bindings
     set -g fish_sequence_key_delay_ms 300
 
-    bind \cx\ce edit_command_buffer
-    bind \cl\ct __fish_git_semtag
+    bind ctrl-c cancel-commandline
+    bind ctrl-x,ctrl-e edit_command_buffer
+    bind ctrl-l,ctrl-t __fish_git_semtag
 end
