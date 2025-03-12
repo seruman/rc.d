b4f908615ee8c3e874fd72651d9a7f62347fa422
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2025-03-12 11:34:49 +0300

    .config/fish: update righprompt

--- private_dot_config/private_fish/functions/fish_right_prompt.fish
+++ private_dot_config/private_fish/functions/fish_right_prompt.fish
@@ -1,5 +1,11 @@
 function fish_right_prompt
+
+    set -l parts
     for item in $fish_right_prompt_items
-        $item
+        set -l got (eval $item)
+        if test -n "$got"
+            echo -n "$got"
+            echo -n " "
+        end
     end
 end
