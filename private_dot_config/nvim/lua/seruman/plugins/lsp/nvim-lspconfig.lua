addc08f0d228f91bb300319302be381ae3b48eef
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2025-04-25 20:25:24 +0300

    .config/nvim/.../nvim-lspconfig: use biome for {js,ts}{x,} files

--- private_dot_config/nvim/lua/seruman/plugins/lsp/nvim-lspconfig.lua
+++ private_dot_config/nvim/lua/seruman/plugins/lsp/nvim-lspconfig.lua
@@ -14,10 +14,10 @@ return {
 				opts = {
 					formatters_by_ft = {
 						lua = { "stylua" },
-						typescript = { "prettierd", "prettier", stop_after_first = true },
-						typescriptreact = { "prettierd", "prettier", stop_after_first = true },
-						javascript = { "prettierd", "prettier", stop_after_first = true },
-						javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+						typescript = { "biome", "prettierd", "prettier", stop_after_first = true },
+						typescriptreact = { "biome", "prettierd", "prettier", stop_after_first = true },
+						javascript = { "biome", "prettierd", "prettier", stop_after_first = true },
+						javascriptreact = { "biome", "prettierd", "prettier", stop_after_first = true },
 						json = { "prettierd", "prettier", stop_after_first = true },
 						html = { "prettierd", "prettier", stop_after_first = true },
 						css = { "prettierd", "prettier", stop_after_first = true },
