722a4a9740617f2e78cbdf39654b9440e43bb184
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2025-09-19 18:32:12 +0300

    .config/fish: add bump mapping to semtag fzf thingy

--- private_dot_config/private_fish/functions/__fish_git_semtag.fish
+++ private_dot_config/private_fish/functions/__fish_git_semtag.fish
@@ -1,20 +1,22 @@
 function __fish_git_semtag
-    # Check if we're in a git repo first
     if not git rev-parse --is-inside-work-tree >/dev/null 2>&1
         return 1
     end
 
-    # Get tags
     set -l tags (git semtag -r -ii -fc)
     if not test -n "$tags"
         return
     end
 
-    # Get selection from fzf
     set -l selection (string split \n -- $tags | \
         fzf --ansi \
             --prompt="semtag> " \
-            --preview="git show --color {1}")
+            --preview="git show --color {1}" \
+            --header="Enter: select | ctrl-p: patch | ctrl-n: minor | ctrl-j: major | ctrl-r: prerelease" \
+            --bind "ctrl-p:become(echo {} | semver-parse bump patch)" \
+            --bind "ctrl-n:become(echo {} | semver-parse bump minor)" \
+            --bind "ctrl-j:become(echo {} | semver-parse bump major)" \
+            --bind "ctrl-r:become(echo {} | semver-parse bump prerelease)")
 
     commandline -f repaint
     if test -n "$selection"
