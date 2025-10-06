77d457e313170f01b73ee677d281d8538961896d
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2025-10-06 20:05:48 +0300

    .config/glide: update

--- private_dot_config/glide/glide.ts
+++ private_dot_config/glide/glide.ts
@@ -1,29 +1,32 @@
 glide.prefs.set("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 glide.prefs.set("devtools.debugger.prompt-connection", false);
-glide.prefs.set(
-	"media.videocontrols.picture-in-picture.audio-toggle.enabled",
-	true,
-);
+glide.prefs.set("media.videocontrols.picture-in-picture.audio-toggle.enabled", true);
 glide.prefs.set("browser.tabs.insertAfterCurrent", false);
 glide.prefs.set("browser.tabs.insertRelatedAfterCurrent", false);
+glide.prefs.set("browser.uidensity", 1);
+glide.prefs.set("browser.startup.page", 3);
+glide.prefs.set("browser.warnOnQuitShortcut", false);
 
-glide.keymaps.set(
-	"normal",
-	"o",
-	async () => {
-		await glide.keys.send("<D-l>");
-	},
-	{ description: "Focus the address bar" },
-);
+glide.g.mapleader = "\\";
+glide.o.hint_size = "16px";
+
+glide.keymaps.set("normal", "o", "keys <D-l>", {
+	description: "Focus the address bar",
+});
+
+glide.keymaps.set("normal", "<S-j>", "tab_next", {
+	description: "Scroll down",
+});
+
+glide.keymaps.set("normal", "<S-k>", "tab_prev", {
+	description: "Scroll down",
+});
 
 glide.keymaps.set(
 	"normal",
 	"<S-o>",
 	async ({ tab_id }) => {
-		await browser.tabs.create({
-			active: true,
-			openerTabId: tab_id,
-		});
+		await browser.tabs.create({ active: true, openerTabId: tab_id });
 		await glide.keys.send("<D-l>");
 	},
 	{ description: "Open a new tab and focus the address bar" },
@@ -57,8 +60,7 @@ glide.keymaps.set(
 glide.autocmds.create("UrlEnter", /https:\/\/github\.com\/.*\/.*/, () => {
 	glide.buf.keymaps.set("normal", "<C-g>p", async ({ tab_id }) => {
 		const path = glide.ctx.url.pathname;
-		const { org, repo } =
-			path.match(/^\/(?<org>[^/]+)\/(?<repo>[^/]+)/)?.groups ?? {};
+		const { org, repo } = path.match(/^\/(?<org>[^/]+)\/(?<repo>[^/]+)/)?.groups ?? {};
 
 		const pkggodev_url = `https://pkg.go.dev/github.com/${org}/${repo}`;
 		await browser.tabs.create({
