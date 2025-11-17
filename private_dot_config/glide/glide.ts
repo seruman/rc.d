377404af018875d6e4c660bf5a3a3b04b46c6f20
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2025-11-17 16:52:48 +0300

    .config/glide: add over engineered github mappings

--- private_dot_config/glide/glide.ts
+++ private_dot_config/glide/glide.ts
@@ -20,6 +20,10 @@ glide.unstable.include("glide.work.ts");
 glide.g.mapleader = "\\";
 glide.o.hint_size = "16px";
 
+// See; https://github.com/glide-browser/glide/discussions/94#discussioncomment-14856522
+glide.keymaps.set(["insert", "normal"], "<C-d>", "keys <pagedown>");
+glide.keymaps.set(["normal", "insert"], "<C-u>", "keys <pageup>");
+
 glide.keymaps.set("normal", "o", "keys <D-l>", {
 	description: "Focus the address bar",
 });
@@ -72,23 +76,230 @@ glide.keymaps.set(
 	{ description: "Open recently closed tab" },
 );
 
-glide.autocmds.create("UrlEnter", /https:\/\/github\.com\/.*\/.*/, () => {
+glide.keymaps.set(
+	"normal",
+	"/",
+	async () => {
+		await glide.keys.send("<d-f>");
+	},
+	{ description: "Search forward" },
+);
+
+type Address = `http://${string}` | `https://${string}`;
+type GitHubParams = {
+	addr: Address;
+	org: string;
+	repo: string;
+	issue?: string;
+};
+
+function github_params(url: URL): GitHubParams | null {
+	const PROJECTS_URL_PATTERN = /https:\/\/github(.*)\.com\/orgs\/(.+)\/projects\/(\d+)/;
+	const DEFAULT_PATTERN = /https:\/\/github(.*)\.com\/(.+)\/(.+)/;
+
+	const addr = `${url.protocol}//${url.host}` as Address;
+
+	if (PROJECTS_URL_PATTERN.test(url.href)) {
+		const match = url.href.match(PROJECTS_URL_PATTERN);
+		if (!match) {
+			return null;
+		}
+
+		const issueQuery = url.searchParams.get("issue") ?? "";
+		if (!issueQuery) {
+			return null;
+		}
+
+		const [org, repo, issue] = issueQuery.split("|");
+
+		if (!org || !repo || !issue) {
+			return null;
+		}
+
+		return { addr, org, repo, issue: issueQuery };
+	}
+
+	if (DEFAULT_PATTERN.test(url.href)) {
+		const match = url.href.match(DEFAULT_PATTERN);
+		if (!match) {
+			return null;
+		}
+
+		const path = url.pathname.slice(1).split("/");
+		if (path.length < 2) {
+			return null;
+		}
+
+		const org = path[0];
+		const repo = path[1];
+
+		if (!org || !repo) {
+			return null;
+		}
+
+		return { addr, org, repo };
+	}
+
+	return null;
+}
+
+// GitHub mappings.
+glide.autocmds.create("UrlEnter", /https:\/\/github(.*)\.com/, () => {
+	const must_github_params = <T>(fn: (params: GitHubParams) => T): T | undefined => {
+		const params = github_params(glide.ctx.url);
+		if (!params) {
+			return;
+		}
+
+		return fn(params);
+	};
+
+	const with_issue_url = (
+		fn: (args: { params: GitHubParams; issue_addr: string }) => void | Promise<void>,
+	): void | Promise<void> => {
+		return must_github_params((params) => {
+			const { addr, org, repo, issue } = params;
+			const repo_addr = `${addr}/${org}/${repo}`;
+			if (!issue) {
+				console.log("No issue specified, opening issues list", { params }, `${repo_addr}/issues`);
+				return fn({ params, issue_addr: `${repo_addr}/issues` });
+			}
+
+			const issue_addr = `${addr}/${org}/${repo}/issues/${issue}`;
+			return fn({ params, issue_addr });
+		});
+	};
+
+	const with_repo_url = (
+		fn: (args: { params: GitHubParams; repo_addr: string }) => void | Promise<void>,
+	): void | Promise<void> => {
+		return must_github_params((params) => {
+			const { addr, org, repo } = params;
+			const repo_addr = `${addr}/${org}/${repo}`;
+			return fn({ params, repo_addr });
+		});
+	};
+
+	const with_prs_url = (
+		fn: (args: { params: GitHubParams; prs_addr: string }) => void | Promise<void>,
+	): void | Promise<void> => {
+		return must_github_params((params) => {
+			const { addr, org, repo } = params;
+			const prs_addr = `${addr}/${org}/${repo}/pulls`;
+			return fn({ params, prs_addr });
+		});
+	};
+
+	glide.buf.keymaps.set(
+		"normal",
+		"<C-g>i",
+		async ({ tab_id }) => {
+			with_issue_url(async ({ issue_addr }) => {
+				console.log(">>>>>>> Navigating to issue:", issue_addr);
+				await browser.tabs.update(tab_id, { url: issue_addr });
+			});
+		},
+		{ description: "Open specific GitHub issue or issue list in the current tab" },
+	);
+
+	glide.buf.keymaps.set(
+		"normal",
+		"<C-g>I",
+		async ({ tab_id }) => {
+			with_issue_url(async ({ issue_addr }) => {
+				await browser.tabs.create({
+					url: issue_addr,
+					active: true,
+					openerTabId: tab_id,
+				});
+			});
+		},
+		{ description: "Open specific GitHub issue or issue list in a new tab" },
+	);
+
+	glide.buf.keymaps.set(
+		"normal",
+		"<C-g>r",
+		async ({ tab_id }) => {
+			with_repo_url(async ({ repo_addr }) => {
+				await browser.tabs.update(tab_id, { url: repo_addr });
+			});
+		},
+		{ description: "Open GitHub repo root in the current tab" },
+	);
+
+	glide.buf.keymaps.set(
+		"normal",
+		"<C-g>R",
+		async ({ tab_id }) => {
+			with_repo_url(async ({ repo_addr }) => {
+				await browser.tabs.create({
+					url: repo_addr,
+					active: true,
+					openerTabId: tab_id,
+				});
+			});
+		},
+		{ description: "Open GitHub repo root in the current tab" },
+	);
+
 	glide.buf.keymaps.set(
 		"normal",
 		"<C-g>p",
 		async ({ tab_id }) => {
-			const path = glide.ctx.url.pathname;
-			const { org, repo } = path.match(/^\/(?<org>[^/]+)\/(?<repo>[^/]+)/)?.groups ?? {};
-
-			const pkggodev_url = `https://pkg.go.dev/github.com/${org}/${repo}`;
-			await browser.tabs.create({
-				url: pkggodev_url,
-				active: true,
-				openerTabId: tab_id,
+			with_prs_url(async ({ prs_addr }) => {
+				await browser.tabs.update(tab_id, { url: prs_addr });
 			});
 		},
-		{ description: "Open pkg.go.dev for this repo" },
+		{ description: "Open GitHub pull requests list in the current tab" },
 	);
+
+	glide.buf.keymaps.set(
+		"normal",
+		"<C-g>P",
+		async ({ tab_id }) => {
+			with_prs_url(async ({ prs_addr }) => {
+				await browser.tabs.create({
+					url: prs_addr,
+					active: true,
+					openerTabId: tab_id,
+				});
+			});
+		},
+		{ description: "Open GitHub pull requests list in a new tab" },
+	);
+
+	if (glide.ctx.url.hostname === "github.com") {
+		glide.buf.keymaps.set(
+			"normal",
+			"<C-g>g",
+			async ({ tab_id }) => {
+				must_github_params(async (params) => {
+					const { org, repo } = params;
+					const url = `https://pkg.go.dev/github.com/${org}/${repo}`;
+					await browser.tabs.update(tab_id, { url });
+				});
+			},
+			{ description: "Open Go package documentation for this GitHub repo" },
+		);
+
+		glide.buf.keymaps.set(
+			"normal",
+			"<C-g>G",
+			async ({ tab_id }) => {
+				must_github_params(async (params) => {
+					const { org, repo } = params;
+					const url = `https://pkg.go.dev/github.com/${org}/${repo}`;
+					await browser.tabs.create({
+						url,
+						active: true,
+						openerTabId: tab_id,
+					});
+				});
+			},
+			{ description: "Open Go package documentation for this GitHub repo in a new tab" },
+		);
+	}
 });
 
 glide.keymaps.set(
@@ -130,3 +341,5 @@ glide.keymaps.set(
 	},
 	{ description: "Focus on Okta extension" },
 );
+
+glide.keymaps.set("normal", "<C-,>", "blur");
