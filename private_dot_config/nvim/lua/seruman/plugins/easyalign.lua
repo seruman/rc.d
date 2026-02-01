6f2e3e53fe7bb29d23a40b12bcbc63a95d9b535f
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2026-02-01 16:45:23 +0300

    .config/nvim: remove easyalign, sg.nvim, .luarc.json, and old lsp/java/ path

--- private_dot_config/nvim/lua/seruman/plugins/easyalign.lua
+++
@@ -1,9 +0,0 @@
-return {
-	{
-		"junegunn/vim-easy-align",
-		keys = {
-			{ "ga", "<Plug>(EasyAlign)", desc = "Align", mode = "x" },
-			{ "ga", "<Plug>(EasyAlign)", desc = "Align", mode = "n" },
-		},
-	},
-}
