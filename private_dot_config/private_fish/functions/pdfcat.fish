17425f4c1bc9da1cc837ec02608067c0078574f6
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2026-02-02 23:49:35 +0300

    .config/fish: add temp file cleanup to pdfcat

--- private_dot_config/private_fish/functions/pdfcat.fish
+++ private_dot_config/private_fish/functions/pdfcat.fish
@@ -1,6 +1,6 @@
 function pdfcat --description 'Preview PDF in terminal using pdfium and timg'
-    # TODO: add cleanup
     set -l tmpfile (mktemp)
     pdfium-cli render $argv[1] - > $tmpfile
     timg --pixelation=kitty -W $tmpfile
+    rm -f $tmpfile
 end
